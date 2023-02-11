d740f3d27443e6fe9713affcc1309647507dcf2a
Salma <salkashir@gmail.com>
2023-02-11 23:10:53 +0200

    Favourites

--- SportsApp/View/Controllers/FavouritesViewController.swift
+++ SportsApp/View/Controllers/FavouritesViewController.swift
@@ -6,29 +6,45 @@
 //
 
 import UIKit
+import CoreData
 
 class FavouritesViewController: UIViewController {
+    
+    var FavouriteViewModel : ViewModel?
+    var FavouriteLeagues : [NSManagedObject]?
 
     @IBOutlet weak var FavouritesTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        FavouriteViewModel = ViewModel()
+        FavouriteLeagues = FavouriteViewModel?.database.fetchFormCoreData()
+        
+        
         FavouritesTableView.delegate = self
         FavouritesTableView.dataSource = self
-        FavouritesTableView.reloadData()
 
         let nib = UINib(nibName: "TableViewCell", bundle: nil)
         FavouritesTableView.register(nib, forCellReuseIdentifier: "TableCell")
+       // self.FavouritesTableView.reloadData()
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        self.FavouritesTableView.reloadData()
+
+    }
 }
 extension FavouritesViewController : UITableViewDelegate , UITableViewDataSource
 {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return FavouriteLeagues?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let FavouriteTablecell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as! TableViewCell
+        
+        FavouriteTablecell.LeagueName.text = (FavouriteLeagues?[indexPath.row].value(forKey: "league_name") as? String)
+        
+        FavouriteTablecell.LeaugeLogo.kf.setImage(with: URL(string: FavouriteLeagues![indexPath.row].value(forKey: "league_logo") as! String),placeholder: UIImage(named: "loading"))
+        
         FavouriteTablecell.LeaugeLogo.layer.cornerRadius = 37
         
         FavouriteTablecell.LeagueName.layer.cornerRadius = 10
@@ -44,3 +60,4 @@ extension FavouritesViewController : UITableViewDelegate , UITableViewDataSource
         return 90
     }
 }
+
